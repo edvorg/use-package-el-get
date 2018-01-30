87f643d607e11783df7574168e6c2d5e18c2767d
Matúš Honěk <matushonek@gmail.com>
2018-01-30 17:16:00 +0100

    fix typo

--- use-package-el-get.el
+++ use-package-el-get.el
@@ -14,7 +14,7 @@
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU Lesser General Public License as
 ;; published by the Free Software Foundation; either version 3, or (at
-n;; your option) any later version.
+;; your option) any later version.
 
 ;; This program is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
